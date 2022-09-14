5645f04ce3502ab1d65dcb111a89f68d249ab8c0
yasmin <thegodha1@gmail.com>
2022-09-14 21:10:12 +0300

    First commit

---
+++ src/root-cmp.jsx
@@ -0,0 +1,25 @@
+import React from 'react'  
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="app">
+            {/* <AppHeader /> */}
+    <main className="container">
+        {/* <Routes>
+            <Route path='' element={<ToyApp />}></Route>
+            <Route path='statistics' element={<MyChart />}></Route>
+            <Route path='about' element={<About />}></Route>
+            <Route path='toy/:id' element={<ToyDetails />}></Route>
+            <Route path='toy/edit' element={<ToyEdit />}></Route>
+            <Route path='toy/edit/:id' element={<ToyEdit />}></Route>
+            <Route path='login' element={<Login />}></Route>
+            <Route path='user' element={<UserDetails />}></Route>
+        </Routes> */}
+        <h1>Hi</h1>
+    </main>
+</div>
+  )  
+}
+
+export default App  

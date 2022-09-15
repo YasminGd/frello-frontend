f6b7fac70a5816ae947eea35881e93c72e008ebf
leesharon <leesharooon@gmail.com>
2022-09-15 14:11:47 +0300

    header added

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
+const logo = require('../assets/img/logo-frello.png')
 
 export const AppHeader = () => {
   return (
     <section className="app-header">
       <div className="main-logo">
-        <img src="../assets/img/logo.png" alt="" />
+        <img src={logo} alt="" />
         <h1>Frello</h1>
       </div>
-      <nav>
+      <nav className="home-nav">
         <Link className="login" to={'user/login'}>Log in</Link>
         <Link className="signup" to={'user/signup'}>Get Frello for free</Link>
       </nav>

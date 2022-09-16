3e5a45592efc2d084aaef3e8fb89788df0240959
ohayonb <ohayonzzb@gmail.com>
2022-09-16 23:41:46 +0300

    setActionModal function sent from details

--- src/cmps/action-modal.jsx
+++ src/cmps/action-modal.jsx
@@ -2,7 +2,7 @@ import { IoCloseOutline } from 'react-icons/io5'
 import { Cover } from './action-modal-cmps/Cover'
 import { Attachment } from './action-modal-cmps/Attachment'
 
-export const ActionModal = ({ data, task, onUpdateTask }) => {
+export const ActionModal = ({ data, task, onUpdateTask, setActionModal }) => {
   const { type, pos } = data
   const modalStyle = { left: pos.left + 'px', top: pos.bottom + 'px' }
 

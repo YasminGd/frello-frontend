3f3dc1ea7aab16125ffedeb1a336545ac86ad56e
yasmin <thegodha1@gmail.com>
2022-09-16 23:31:14 +0300

    finished color cover

--- src/cmps/task-preview.jsx
+++ src/cmps/task-preview.jsx
@@ -1,10 +1,21 @@
 import { Link } from "react-router-dom";
 
 export const TaskPreview = ({ task, groupId, removeItem }) => {
+    const getCoverStyle = () => {
+        if (task.style?.coverStyle) {
+            if (task.style.coverStyle === 'fully covered') {
+                return task.style.bgColor ? task.style.bgColor : ''
+            }
+        }
+        return ''
+    }
+
+    console.log(getCoverStyle());
     return (<Link to={`${groupId}/${task.id}`} className="task-preview">
         {task.style?.bgColor && <section className="cover-color" style={{ backgroundColor: task.style.bgColor }}>
         </section>}
-        <section className="task-body">
+        <section className="task-body"
+            style={{ backgroundColor: getCoverStyle() }}>
             {task.title}
         </section>
     </Link>)

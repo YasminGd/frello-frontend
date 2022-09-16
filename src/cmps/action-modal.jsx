27c57a95ccbae8b8ca35a31ddfb7fcbe02b4ebc6
ohayonb <ohayonzzb@gmail.com>
2022-09-16 15:39:29 +0300

    action modal update

--- src/cmps/action-modal.jsx
+++ src/cmps/action-modal.jsx
@@ -1,6 +1,15 @@
 import { IoCloseOutline } from 'react-icons/io5'
 
 export const ActionModal = ({ type }) => {
+  const getActionCmp = (type) => {
+    switch (type) {
+      case '':
+        break
+
+      default:
+        break
+    }
+  }
   return (
     <section className="action-modal">
       <div className="title-container">

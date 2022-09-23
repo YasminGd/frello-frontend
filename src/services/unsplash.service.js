96b90b02e4964ccd5d2c6b473c2110d6717681d0
YasminGd <thegodha1@gmail.com>
2022-09-23 03:50:50 +0300

    fixed bug

--- src/services/unsplash.service.js
+++ src/services/unsplash.service.js
@@ -10,17 +10,17 @@ const photos = _loadFromStorage(KEY) || null
 async function getPhotos(searchWords) {
     // Defining our variables
     console.log(searchWords);
-    if(!searchWords && photos) return photos
+    if (!searchWords && photos) return photos
     const ACCESS_KEY = 'vuoea2539QLM1SmLGMMoXzzUFtAGa1No7X6sFclUQa4'
-    let URL = `https://api.unsplash.com/search/photos?page=1&per_page=30&query=${searchWords}&client_id=${ACCESS_KEY}`
-    // let URL = `${BASE_URL}?client_id=${ACCESS_KEY}`
-    // if(searchWords) URL = URL + `/topics/${searchWords}/photos?page=1&per_page=14`
+    let URL = `https://api.unsplash.com/photos/random?count=30${searchWords ?`&query=${searchWords}`: ''}&client_id=${ACCESS_KEY}`
+    // let URL = `https://api.unsplash.com/search/photos?page=1&per_page=30${searchWords ?`&query=${searchWords}`: ''}&client_id=${ACCESS_KEY}`
     try {
+        console.log(URL);
         const response = await axios.get(URL)
-        const {data} = response
+        const { data } = response
         console.log(data);
-        const photos = data.results.map((photo) =>( {backgroundColor: photo.color, background:photo.urls.full, thumbnail: photo.urls.small }))
-        _saveToStorage(KEY,photos)
+        const photos = data.map((photo) => ({ backgroundColor: photo.color, background: photo.urls.full, thumbnail: photo.urls.small }))
+        _saveToStorage(KEY, photos)
         return photos
     } catch (err) {
         console.error('ERROR!', err)

b24ea00bc4c95da29f177bc54faacec4fe10a997
ohayonb <ohayonzzb@gmail.com>
2022-09-29 19:43:41 +0300

    dashboard edited

--- src/cmps/board/dashboard.jsx
+++ src/cmps/board/dashboard.jsx
@@ -1,27 +1,49 @@
-import { IoCloseOutline } from "react-icons/io5"
-import { useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
-import { Doughnut } from 'react-chartjs-2'
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-import { dashboardService } from "../../services/dashboard.service"
-ChartJS.register(ArcElement, Tooltip, Legend)
+import { IoCloseOutline } from 'react-icons/io5'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { Doughnut, Line } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  ArcElement,
+  Tooltip,
+  Legend,
+  Title,
+} from 'chart.js'
+import { dashboardService } from '../../services/dashboard.service'
+ChartJS.register(ArcElement, CategoryScale, LinearScale, Title, PointElement, LineElement, Tooltip, Legend)
 
 export const Dashboard = () => {
-    const navigate = useNavigate()
-    const board = useSelector(state => state.boardModule.board)
-    const tasksByStatusData = dashboardService.getTasksByStatus(board.groups)
+  const navigate = useNavigate()
+  const board = useSelector((state) => state.boardModule.board)
+  const tasksByStatusData = dashboardService.getTasksByStatus(board.groups)
+  const tasksByGroupsData = dashboardService.getTasksByGroups(board.groups)
 
-    return (
-        <section className="dashboard">
-            <button className='btn-close' onClick={() => { navigate(-1) }}><IoCloseOutline /></button>
-            <h1>{board.title}</h1>
+  return (
+    <section className="dashboard">
+      <button
+        className="btn-close"
+        onClick={() => {
+          navigate(-1)
+        }}
+      >
+        <IoCloseOutline />
+      </button>
+      <h1>{board.title}</h1>
 
-            <div className="charts-container">
-                <div className="task-per-status">
-                    <h3>Tasks by status</h3>
-                    <Doughnut data={tasksByStatusData} />
-                </div>
-            </div>
-        </section>
-    )
+      <div className="charts-container">
+        <div className="task-per-status">
+          <h3>Tasks by status</h3>
+          <Doughnut data={tasksByStatusData} />
+        </div>
+        <div className="task-per-groups">
+          <h3>Tasks by groups</h3>
+          <Line data={tasksByGroupsData} />
+        </div>
+      </div>
+    </section>
+  )
 }
--- src/services/dashboard.service.js
+++ src/services/dashboard.service.js
@@ -1,44 +1,57 @@
-
 export const dashboardService = {
-    getTasksByStatus
+  getTasksByStatus,
+  getTasksByGroups,
 }
 
 function getTasksByStatus(groups) {
-    const labels = [
-        'Overdue',
-        'Due soon',
-        'On time',
-        'No due date',
-        'Completed',
-    ]
-    const data = { overdue: 0, dueSoon: 0, onTime: 0, noDueDate: 0, completed: 0 }
-    groups.forEach(group => {
-        group.tasks.forEach(task => {
-            if (!task.dueDate) data.noDueDate += 1
-            else if (task.dueDate.isDone) data.completed += 1
-            else data[calcDueStatus(task.dueDate.date)] += 1
-        })
+  const labels = ['Overdue', 'Due soon', 'On time', 'No due date', 'Completed']
+  const data = { overdue: 0, dueSoon: 0, onTime: 0, noDueDate: 0, completed: 0 }
+  groups.forEach((group) => {
+    group.tasks.forEach((task) => {
+      if (!task.dueDate) data.noDueDate += 1
+      else if (task.dueDate.isDone) data.completed += 1
+      else data[calcDueStatus(task.dueDate.date)] += 1
     })
-    const dataValues = Object.values(data)
+  })
+  const dataValues = Object.values(data)
 
-    return {
-        labels: labels,
-        datasets: [{
-            label: 'Tasks Per Status',
-            backgroundColor: ['#EB5B46', '#EDD747', '#288fca', '#A0C2F6', '#78BB5d'],
-            borderColor: ['#F9FAFC'],
-            data: dataValues,
-        }]
-    }
+  return {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Tasks Per Status',
+        backgroundColor: ['#EB5B46', '#EDD747', '#288fca', '#A0C2F6', '#78BB5d'],
+        borderColor: ['#F9FAFC'],
+        data: dataValues,
+      },
+    ],
+  }
+}
+
+function getTasksByGroups(groups) {
+  const labels = groups.map((group) => group.title)
+  const taskCounter = groups.map((group) => group.tasks.length)
+
+  return {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Tasks Per Groups',
+        backgroundColor: ['#EB5B46', '#EDD747', '#288fca', '#A0C2F6', '#78BB5d'],
+        borderColor: ['#F9FAFC'],
+        data: taskCounter,
+      },
+    ],
+  }
 }
 
 function calcDueStatus(dueDate) {
-    const then = new Date(dueDate)
-    const now = new Date()
-    const msBetweenDates = then.getTime() - now.getTime()
-    const hoursBetweenDates = msBetweenDates / (60 * 60 * 1000)
+  const then = new Date(dueDate)
+  const now = new Date()
+  const msBetweenDates = then.getTime() - now.getTime()
+  const hoursBetweenDates = msBetweenDates / (60 * 60 * 1000)
 
-    if (hoursBetweenDates < 0) return 'overdue'
-    else if (hoursBetweenDates < 24) return 'dueSoon'
-    else return 'onTime'
+  if (hoursBetweenDates < 0) return 'overdue'
+  else if (hoursBetweenDates < 24) return 'dueSoon'
+  else return 'onTime'
 }
